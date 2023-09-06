142b3acff56bba965fea1225150b13ff347a4c5c
IanTGW <ianwuxiinau@gmail.com>
2023-09-07 03:38:00 +1000

    Create function to eval integer powers

--- returnvalue/function min(num1, num2){.js
+++ returnvalue/function min(num1, num2){.js
@@ -9,3 +9,22 @@ function min(num1, num2){
 }
 
 console.log(min(3,6))
+
+function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  let x = prompt("x?", '');
+  let n = prompt("n?", '');
+  
+  if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`);
+  } else {
+    alert( pow(x, n) );
+  }
